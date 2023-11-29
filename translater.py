445e4679db1e07a24bdecb6edf02eeb3e189bd96
YoussefAnas <145559720+YoussefAnas@users.noreply.github.com>
2023-11-29 19:13:29 +0200

    Update translater.py

--- translater.py
+++ translater.py
@@ -17,4 +17,7 @@ def text_to_morse(text):
             morse_code += MORSE_CODE_DICT[char] + ' '
         elif char == ' ':
             morse_code += '/'
+        else:
+            print(f"Invalid input: '{char}' is not a valid character.")
+            return None
     return morse_code
