6a67108ac4314f0a689c67388a1a44b0ecad3315
YoussefAnas <145559720+YoussefAnas@users.noreply.github.com>
2023-11-30 16:22:04 +0200

    Update translater.py

--- translater.py
+++ translater.py
@@ -21,3 +21,19 @@ def text_to_morse(text):
             print(f"Invalid input: '{char}' is not a valid character.")
             return None
     return morse_code
+
+def morse_to_text(morse_code):
+    morse_code = morse_code.split(' ')
+    text = ''
+    for code in morse_code:
+        if code in MORSE_CODE_DICT.values():
+            for key, value in MORSE_CODE_DICT.items():
+                if code == value:
+                    text += key
+        elif code == '/':
+            text += ' '
+        else:
+            print(f"Invalid input: '{code}' is not a valid Morse code.")
+            return None
+    return text
+
